a7e0617412de66fcee33eb9a15f7a73e02b7f215
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-22 18:04:57 +0600

    default js and template js practice done

---
+++ js/default.js
@@ -0,0 +1,27 @@
+function add(first , second){
+    console.log(first, second);
+    second = second || 0;
+
+    if(second === undefined){
+        second = 0;
+    }
+    const total = first + second;
+    return total;
+}
+
+
+// ES6
+function add(first , second = 0){
+    const total = first + second;
+    return total;
+}
+
+const result = add(10);
+console.log(result);
+
+function fullName(first, last = "chowdhury"){
+    const name = first + ' ' + last;
+    return name;
+}
+const name = fullName("Gelam");
+console.log(name);
---
+++ js/template.js
@@ -0,0 +1,24 @@
+const person = "adam sand";
+const person2 = "Ben white";
+const person3 = "Donald Trump";
+
+const multiLine = "First Line text \n" + 
+"second line code \n" + 
+"Third line of text \n" + 
+"Fourth line of string";
+// console.log(multiLine);
+
+const newMultiLine = `First line od text
+second line of text
+third line of code
+fourth line of code`;
+// console.log(newMultiLine);
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 45, 12, 6, 444];
+const summary = "sum of: " + a + " and " + b + " is: " + (a+b);
+// console.log(summary);
+
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(newSummary);
