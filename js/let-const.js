5c80fbc59ec1375720545445f27a00a677e97aff
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-22 17:44:34 +0600

    let const some code practice

---
+++ js/let-const.js
@@ -0,0 +1,39 @@
+// breakup with var 
+// no more use of var 
+
+// let: let it to reassign 
+// const: do not allow it to reassign 
+
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = "jan pakhi";
+// bird = "moya pakhi"
+const message = bird + "potas potas";
+console.log(message);
+
+// const numbers = [12, 89, 65, 45];
+// reassign is not allowed 
+// numbers = [77, 66, 55, 22, 99];
+
+let numbers = [12, 89, 65, 45];
+numbers.push(123);
+numbers[1] = 90;
+console.log(numbers);
+
+const student = {
+    name: "mofiz",
+    address: "Rongpur"
+}
+
+// student = {name: "MOfazzol"}
+student.name = "Mofazzol";
+console.log(student);
+
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+    const number = numbers[i];
+    sum = sum + number;
+};
+console.log(sum);
