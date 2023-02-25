ef359928ce971afab1ba1d9f4275a41580692f64
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-25 19:18:13 +0600

    find , reduce & notation js code done

---
+++ more-es6/find.js
@@ -0,0 +1,20 @@
+/* filter er best friend hocche find. filter r fine er moddhe 90% mil royece 
+like:- filter hocche jeta jeta milbe sob gulake dibe, r
+find hocche jeta milbe oitar 1st take dibe then tar kaj ses  */
+
+const numbers = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+
+const fives = numbers.find(num => num % 5 === 0);
+const fivesAll = numbers.filter(num => num % 5 === 0);
+console.log(fives);
+console.log(fivesAll);
+
+const products = [
+    {id: 1, name: "laptop" , price: 45000},
+    {id: 2, name: "mobile" , price: 80000},
+    {id: 3, name: "watch" , price: 35000},
+    {id: 4, name: "tablet" , price: 23000},
+];
+
+const cheap = products.find(product => product.price < 40000);
+console.log(cheap);
