0026fa42337c83e2ff22dd16730c3f53ff057684
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-25 19:52:44 +0600

    more-es6 js summary comment writing done

---
+++ more-es6/summay.js
@@ -0,0 +1,12 @@
+// 1. Optional chaining
+// student.marks? .math;
+
+// 2. map, filter m find
+
+// 3. (optional): forEach, reduce (advanced);
+
+// 4. class 
+
+// 5. (optional) inheritance, super  
+
+// 6. (must)prototypical Inheritance 
